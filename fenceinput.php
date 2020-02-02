a1c38117dae56751384550f56f012b1a892713a0
Alex Stone Lunde <alexlunde@gmail.com>
2020-02-02 21:43:16 +0000

    separated functions into separate file

---
+++ fenceinput.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+?>
+
+
+<html>
+<body>
+<div class="outer">
+    <form>
+        Tick box and enter a distance value if you would like to use the fence railings/posts calculator.
+        <br />
+        <input type="checkbox" name="selectRails" />
+        <br />
+        <input type="text" name="distanceInput" /> (in centimetres)
+        <br />
+
+        <br /><br />
+        ******
+        <br /><br />
+        Tick box and enter railings and posts values if you would like to use the fence length calculator.
+        <br />
+        <input type="checkbox" name="selectRails" />
+        <br />
+        <input type="text" name="railingsInput" /> Number of railings available (in whole units)
+        <br />
+        <input type="text" name="postsInput" /> Number of posts available (in whole units)
+        <br /><br />
+        <input type="submit" name="submitButton" value="Submit"/>
+        <br /><br />
+        REMEMBER: length of railing = 1500cm; width of post = 10cm.
+        </form>
+</div>
+
+</body>
+</html>
